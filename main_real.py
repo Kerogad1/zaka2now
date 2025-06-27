c35bcdb6f6ed1c6139e3917e13557f213486e2ec
Kerogad1 <156355677+Kerogad1@users.noreply.github.com>
2025-06-27 18:56:19 +0300

    Update main_real.py

--- main_real.py
+++ main_real.py
@@ -23,7 +23,7 @@ def slugify(text):
 
 for category, feed_url in rss_feeds.items():
     feed = feedparser.parse(feed_url)
-    for entry in feed.entries[:1]:
+    for entry in feed.entries[:3]:  # أو [:5] لو حابب
         title = entry.title
         link = entry.link
         content = entry.summary if hasattr(entry, 'summary') else ''
